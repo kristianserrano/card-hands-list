226f259813f72731a424df1af4535049072f9cac
Kristian Serrano <kristianserrano@gmail.com>
2023-05-08 19:11:59 -0400

    Changes for abstracting code for other decks

--- src/scripts/adventure-hand-bars-module.js
+++ src/scripts/adventure-hand-bars-module.js
@@ -13,6 +13,7 @@ const ADVENTURE_DECK_MODULE = {
 const ADVENTURE_HAND_BARS_MODULE = {
   adventureBarsList: new Array(),
   id: "adventure-hand-bars",
+  translationPrefix: 'ADVENTURE_HAND_BARS',
   render: async function () {
     const availableAdventureHands = game.cards.filter((c) => c.type === 'hand' && c.getFlag(ADVENTURE_DECK_MODULE.id, 'group') === 'adventure hands' && c.testUserPermission(game.user, "OBSERVER"));
     let discardPile = game.cards.getName(game.settings.get(ADVENTURE_DECK_MODULE.id, 'dumpPileName'));
@@ -23,16 +24,19 @@ const ADVENTURE_HAND_BARS_MODULE = {
     }
 
     // Get the container for the module UI
-    const adventureHandBarElem = document.getElementById('adventure-hand-bars-container');
+    const adventureHandBarElem = document.getElementById(`${this.id}-container`);
 
     // Get the hidden state of the container from the settings
-    const hidden = game.settings.get(ADVENTURE_HAND_BARS_MODULE.id, 'hideBars');
+    const hidden = game.settings.get(this.id, 'hideBars');
     // Render the template
-    const adventureHandBar = await renderTemplate('modules/adventure-hand-bars/templates/adventure-hand-bars-container.hbs', {
+    const templatePath = `modules/${this.id}/templates/${this.id}-container.hbs`;
+    const adventureHandBar = await renderTemplate(templatePath, {
       hands: availableAdventureHands,
       discardPile: discardPile,
       hidden: hidden,
-      isGM: game.user.isGM
+      isGM: game.user.isGM,
+      moduleId: this.id,
+      translationPrefix: this.translationPrefix,
     });
 
     // If the container is in the DOM...
@@ -50,7 +54,7 @@ const ADVENTURE_HAND_BARS_MODULE = {
     /* Set up listeners for hand bar UI */
 
     // Get all the bars and loop through them
-    const bars = document.querySelectorAll('.adventure-hand-bar-cards');
+    const bars = document.querySelectorAll(`.${this.id}-cards`);
 
     // For each bar...
     for (const b of bars) {
@@ -58,46 +62,42 @@ const ADVENTURE_HAND_BARS_MODULE = {
       const hand = await game.cards.get(b.dataset.handId);
 
       // Set the element ID to use for query selectors
-      const barElemId = `#adventure-hand-bar-${hand.id}`;
+      const barElemId = `#${this.id}-${hand.id}`;
 
       // Add listener for opening the hand sheet when clicking on the hand name
-      const handNameElem = document.querySelector(`${barElemId} .adventure-hand-bar-name`);
+      const handNameElem = document.querySelector(`${barElemId} .${this.id}-name`);
       handNameElem.addEventListener('click', async (e) => {
         await hand.sheet.render(true);
       });
 
       // Add listener for opening the hand sheet when clicking on the cards container
-      const handContainer = document.querySelector(`${barElemId} .adventure-hand-bar-cards-container`);
+      const handContainer = document.querySelector(`${barElemId} .${this.id}-cards-container`);
       handContainer.addEventListener('click', async (e) => {
         await hand.sheet.render(true);
       });
 
       // Add listener for drawing an Adventure Card
-      const drawCardButton = document.querySelector(`${barElemId} .adventure-hand-bar-draw`);
+      const drawCardButton = document.querySelector(`${barElemId} .${this.id}-draw`);
       // This button only appears for those with ownership permission, so check if it exists
       if (drawCardButton) {
         drawCardButton.addEventListener('click', async (e) => {
-          await ADVENTURE_HAND_BARS_MODULE.drawCard(e);
+          await this.drawCard(e);
         });
       }
     }
 
     // Add listener for hiding and showing the module UI container when the icon is clicked
-    document.querySelector('.adventure-hand-bar-hide-show').addEventListener('click', async () => {
-      const adventureHandBarElem = document.getElementById('adventure-hand-bars-container');
+    document.querySelector(`#${this.id}-container h3`).addEventListener('click', async () => {
+      const adventureHandBarElem = document.getElementById(`${this.id}-container`);
       adventureHandBarElem.classList.toggle('hidden');
-      const arrow = document.querySelector('.adventure-hands-mode');
+      const arrow = document.querySelector(`.${this.id}-mode`);
       arrow.classList.toggle('fa-angle-up');
       arrow.classList.toggle('fa-angle-down');
-      await game.settings.set(ADVENTURE_HAND_BARS_MODULE.id, 'hideBars', adventureHandBarElem.classList.contains('hidden'))
+      await game.settings.set(this.id, 'hideBars', adventureHandBarElem.classList.contains('hidden'))
     });
   },
   //Opens the hand for any additional options
   openHand: async function (hand) {
-    if (hand == undefined) {
-      ui.notifications.warn(game.i18n.localize("ADVENTUREHANDBARS.NoHandSelected"));
-      return;
-    }
     if (hand.sheet.rendered) {
       hand.sheet.close();
     } else {
@@ -113,9 +113,9 @@ const ADVENTURE_HAND_BARS_MODULE = {
     var newlyDrawn = (await deck.deal([hand], 1)).drawnCards.filter(n => !prevDrawn.includes(n));
 
     // Use adventure deck chat templates.
-    if (game.settings.get("adventure-deck", "announceCards")) { //AnnounceCards?
+    if (game.settings.get(ADVENTURE_DECK_MODULE.id, "announceCards")) { //AnnounceCards?
       // Prerender chatcard.
-      const message = await renderTemplate("modules/adventure-deck/templates/dealtcards-chatcard.hbs", {
+      const message = await renderTemplate(`modules/${ADVENTURE_DECK_MODULE.id}/templates/dealtcards-chatcard.hbs`, {
         player: hand.name,
         cards: newlyDrawn
       });
@@ -141,7 +141,7 @@ Hooks.on("init", function () {
 
 Hooks.on("ready", function () {
   // Pre Load templates.
-  loadTemplates(['modules/adventure-hand-bars/templates/adventure-hand-bars-container.hbs']);
+  loadTemplates([`modules/${ADVENTURE_HAND_BARS_MODULE.id}/templates/${ADVENTURE_HAND_BARS_MODULE.id}-container.hbs`]);
   // Get the adventure deck using the stored name in the adventure cards module.
   const adventureDeck = game.cards.getName(game.settings.get(ADVENTURE_DECK_MODULE.id, 'deckName'));
   // Check if default ownership is Limited for card draws. If it's not, set it.
